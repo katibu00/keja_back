ebc7991cced89a68d5c7d4ccc95684ccde1d588c
katibu01 <ukmisau@gmail.com>
2024-06-04 21:50:16 +0100

    “update”

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ use App\Models\PurchaseTransaction;
 use App\Models\ReservedAccount;
 use App\Models\User;
 use App\Models\Wallet;
-use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -31,6 +30,7 @@ class HomeController extends Controller
         return view('home.regular', compact('accounts', 'popUp', 'funding_charges_description', 'bonusPerGB', 'marqueeNotification'));
     }
 
+
     // public function admin()
     // {
     //     $totalUsers = User::count();
@@ -39,7 +39,7 @@ class HomeController extends Controller
     //     $totalWalletBalance = Wallet::sum('main_balance');
 
     //     $todayTotalFunding = FundingTransaction::whereDate('created_at', today())->sum('amount');
-    //     $TotalFunding = FundingTransaction::sum('amount');
+    //     $TotalFunding= FundingTransaction::sum('amount');
 
     //     $dataPurchases = PurchaseTransaction::where('purchase_type', 'data')
     //         ->whereDate('created_at', today())
@@ -56,142 +56,111 @@ class HomeController extends Controller
 
     //     $newRegularUsers = User::whereDate('created_at', today())->where('user_type', 'regular')->get();
 
-    //     $startDate = Carbon::now()->subDays(15);
-    //     $endDate = Carbon::now();
-
-    //     // Data for the last 15 days
-    //     $dataPurchasesLast15Days = PurchaseTransaction::where('purchase_type', 'data')
-    //         ->whereBetween('created_at', [$startDate, $endDate])
-    //         ->get();
-
-    //     $dataPurchasesByDate = $dataPurchasesLast15Days->groupBy(function ($purchase) {
-    //         return $purchase->created_at->format('Y-m-d');
-    //     });
-
-    //     $dates = [];
-    //     $totalDataPurchaseInGB = [];
-
-    //     foreach ($dataPurchasesByDate as $date => $purchases) {
-    //         $dates[] = $date;
-    //         $totalPurchase = 0;
-    //         foreach ($purchases as $purchase) {
-    //             $dataPlan = DataPlan::where('plan_id', $purchase->data_plan_id)->first();
-    //             if ($dataPlan) {
-    //                 $totalPurchase += $this->decodeAmount($dataPlan->amount);
-    //             }
-    //         }
-    //         $totalDataPurchaseInGB[] = $totalPurchase;
-    //     }
-
-    //     $fundingLast15Days = FundingTransaction::whereBetween('created_at', [$startDate, $endDate])
-    //         ->groupBy('date')
-    //         ->orderBy('date')
-    //         ->get([
-    //             \DB::raw('DATE(created_at) as date'),
-    //             \DB::raw('SUM(amount) as total_amount'),
-    //         ]);
-
-    //     $registrationsLast15Days = User::whereBetween('created_at', [$startDate, $endDate])
-    //         ->groupBy('date')
-    //         ->orderBy('date')
-    //         ->get([
-    //             \DB::raw('DATE(created_at) as date'),
-    //             \DB::raw('COUNT(id) as total_users'),
-    //         ]);
-
-    //     // Prepare data for Chart.js
-    //     $dates = [];
-    //     $dataPurchases = [];
-    //     $funding = [];
-    //     $registrations = [];
-
-    //     foreach ($dataPurchasesLast15Days as $dataPurchase) {
-    //         $dates[] = $dataPurchase->date;
-    //         $dataPurchases[] = $dataPurchase->total_amount;
-    //     }
-
-    //     foreach ($fundingLast15Days as $fund) {
-    //         $funding[] = $fund->total_amount;
-    //     }
-
-    //     foreach ($registrationsLast15Days as $registration) {
-    //         $registrations[] = $registration->total_users;
-    //     }
-
-    //     return view('admin.home', compact('totalUsers', 'registeredToday', 'totalWalletBalance', 'todayTotalFunding', 'TotalFunding', 'totalDataPurchaseInGB', 'todayAirtimePurchase', 'newRegularUsers', 'dates', 'dataPurchases', 'funding', 'registrations'));
+    //     return view('admin.home', compact('totalUsers','registeredToday', 'totalWalletBalance', 'todayTotalFunding', 'TotalFunding','totalDataPurchaseInGB', 'todayAirtimePurchase', 'newRegularUsers'));
     // }
 
 
+    // private function decodeAmount($amount)
+    // {
+    //     preg_match('/\d+/', $amount, $matches);
+    //     if (!empty($matches)) {
+    //         $value = intval($matches[0]);
+
+    //         if (strpos($amount, 'GB') !== false) {
+    //             return $value;
+    //         } elseif (strpos($amount, 'MB') !== false) {
+    //             return $value / 1000;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+
+
     public function admin()
-    {
-        $totalUsers = User::count();
-        $registeredToday = User::whereDate('created_at', today())->count();
-    
-        $totalWalletBalance = Wallet::sum('main_balance');
-    
-        $todayTotalFunding = FundingTransaction::whereDate('created_at', today())->sum('amount');
-        $TotalFunding = FundingTransaction::sum('amount');
-    
-        $dataPurchasesLast15Days = PurchaseTransaction::where('purchase_type', 'data')
-            ->whereBetween('created_at', [now()->subDays(15), now()])
+{
+    $totalUsers = User::count();
+    $registeredToday = User::whereDate('created_at', today())->count();
+
+    $totalWalletBalance = Wallet::sum('main_balance');
+
+    $todayTotalFunding = FundingTransaction::whereDate('created_at', today())->sum('amount');
+    $TotalFunding = FundingTransaction::sum('amount');
+
+    $dataPurchases = PurchaseTransaction::where('purchase_type', 'data')
+        ->whereDate('created_at', today())
+        ->get();
+    $totalDataPurchaseInGB = 0;
+    foreach ($dataPurchases as $purchase) {
+        $dataPlan = DataPlan::where('plan_id', $purchase->data_plan_id)->first();
+        $totalDataPurchaseInGB += $dataPlan ? $this->decodeAmount($dataPlan->amount) : 0;
+    }
+
+    $todayAirtimePurchase = PurchaseTransaction::where('purchase_type', 'airtime')
+        ->whereDate('created_at', today())
+        ->sum('data_plan_id');
+
+    $newRegularUsers = User::whereDate('created_at', today())->where('user_type', 'regular')->get();
+
+    // Data for the graphs
+    $dates = [];
+    $userRegistrations = [];
+    $dataPurchasesInGB = [];
+    $fundingTransactions = [];
+
+    for ($i = 0; $i < 15; $i++) {
+        $date = today()->subDays($i)->toDateString();
+        $dates[] = $date;
+        $userRegistrations[] = User::whereDate('created_at', $date)->count();
+
+        $dailyDataPurchases = PurchaseTransaction::where('purchase_type', 'data')
+            ->whereDate('created_at', $date)
             ->get();
-    
-        $dataPurchasesByDate = $dataPurchasesLast15Days->groupBy(function ($purchase) {
-            return $purchase->created_at->format('Y-m-d');
-        });
-    
-        $dates = [];
-        $totalDataPurchaseInGB = [];
-    
-        foreach ($dataPurchasesByDate as $date => $purchases) {
-            $dates[] = $date;
-            $totalPurchase = 0;
-            foreach ($purchases as $purchase) {
-                $dataPlan = DataPlan::where('plan_id', $purchase->data_plan_id)->first();
-                if ($dataPlan) {
-                    $totalPurchase += $this->decodeAmount($dataPlan->amount);
-                }
-            }
-            $totalDataPurchaseInGB[] = $totalPurchase;
+        $dailyDataPurchaseInGB = 0;
+        foreach ($dailyDataPurchases as $purchase) {
+            $dataPlan = DataPlan::where('plan_id', $purchase->data_plan_id)->first();
+            $dailyDataPurchaseInGB += $dataPlan ? $this->decodeAmount($dataPlan->amount) : 0;
         }
-    
-        $fundingLast15Days = FundingTransaction::whereBetween('created_at', [now()->subDays(15), now()])
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get([
-                \DB::raw('DATE(created_at) as date'),
-                \DB::raw('SUM(amount) as total_amount'),
-            ]);
-    
-        $registrationsLast15Days = User::whereBetween('created_at', [now()->subDays(15), now()])
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get([
-                \DB::raw('DATE(created_at) as date'),
-                \DB::raw('COUNT(id) as total_users'),
-            ]);
-    
-        $dataPurchases = $dataPurchasesLast15Days->pluck('total_amount')->toArray();
-        $funding = $fundingLast15Days->pluck('total_amount')->toArray();
-        $registrations = $registrationsLast15Days->pluck('total_users')->toArray();
-    
-        return view('admin.home', compact('totalUsers', 'registeredToday', 'totalWalletBalance', 'todayTotalFunding', 'TotalFunding', 'totalDataPurchaseInGB', 'dates', 'dataPurchases', 'funding', 'registrations'));
+        $dataPurchasesInGB[] = $dailyDataPurchaseInGB;
+
+        $fundingTransactions[] = FundingTransaction::whereDate('created_at', $date)->sum('amount');
     }
-    
 
-    private function decodeAmount($amount)
-    {
-        preg_match('/\d+/', $amount, $matches);
-        if (!empty($matches)) {
-            $value = intval($matches[0]);
+    $dates = array_reverse($dates);
+    $userRegistrations = array_reverse($userRegistrations);
+    $dataPurchasesInGB = array_reverse($dataPurchasesInGB);
+    $fundingTransactions = array_reverse($fundingTransactions);
 
-            if (strpos($amount, 'GB') !== false) {
-                return $value;
-            } elseif (strpos($amount, 'MB') !== false) {
-                return $value / 1000;
-            }
+    return view('admin.home', compact(
+        'totalUsers',
+        'registeredToday',
+        'totalWalletBalance',
+        'todayTotalFunding',
+        'TotalFunding',
+        'totalDataPurchaseInGB',
+        'todayAirtimePurchase',
+        'newRegularUsers',
+        'dates',
+        'userRegistrations',
+        'dataPurchasesInGB',
+        'fundingTransactions'
+    ));
+}
+
+private function decodeAmount($amount)
+{
+    preg_match('/\d+/', $amount, $matches);
+    if (!empty($matches)) {
+        $value = intval($matches[0]);
+
+        if (strpos($amount, 'GB') !== false) {
+            return $value;
+        } elseif (strpos($amount, 'MB') !== false) {
+            return $value / 1000;
         }
-        return false;
     }
+    return false;
+}
+
 
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,188 +3,254 @@
 @section('content')
 
 <main id="main-container">
-    <div class="content">
-        <div class="row">
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-users fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">{{ $totalUsers }}</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Total Users</div>
-                        </div>
-                    </div>
-                </a>
+
+  <div class="content">
+    <div class="row">
+
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-users fa-2x opacity-25"></i>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-users fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">{{ $registeredToday }}</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Registered Today</div>
-                        </div>
-                    </div>
-                </a>
+            <div>
+              <div class="fs-3 fw-semibold">{{ $totalUsers }}</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Total Users</div>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-wallet fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">&#x20A6;{{ @$totalWalletBalance }}</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Total Wallet Balance</div>
-                        </div>
-                    </div>
-                </a>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-users fa-2x opacity-25"></i>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-money fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">&#x20A6;{{ @$todayTotalFunding }}</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Total Funding</div>
-                        </div>
-                    </div>
-                </a>
+            <div>
+              <div class="fs-3 fw-semibold">{{ $registeredToday }}</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Registered Today</div>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-database fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                          @if(!empty($totalDataPurchaseInGB))
-                          <div class="fs-3 fw-semibold">{{ $totalDataPurchaseInGB[0] }} GB</div>
-                      @else
-                          <div class="fs-3 fw-semibold">0 GB</div>
-                      @endif
-                         <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Data Purchase</div>
-                        </div>
-                    </div>
-                </a>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-wallet fa-2x opacity-25"></i>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-phone fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">&#x20A6;-</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Airtime Purchase</div>
-                        </div>
-                    </div>
-                </a>
+            <div>
+              <div class="fs-3 fw-semibold">&#x20A6;{{ @$totalWalletBalance }}</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Total Wallet Balance</div>
             </div>
-            <div class="col-6 col-xl-3">
-                <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
-                    <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
-                        <div class="d-none d-sm-block">
-                            <i class="fa fa-money fa-2x opacity-25"></i>
-                        </div>
-                        <div>
-                            <div class="fs-3 fw-semibold">&#x20A6;{{ @$TotalFunding }}</div>
-                            <div class="fs-sm fw-semibold text-uppercase text-muted">Total Funding</div>
-                        </div>
-                    </div>
-                </a>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-money fa-2x opacity-25"></i>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <canvas id="dataPurchasesChart"></canvas>
+            <div>
+              <div class="fs-3 fw-semibold">&#x20A6;{{ @$todayTotalFunding }}</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Total Funding</div>
             </div>
-            <div class="col-md-6">
-                <canvas id="fundingChart"></canvas>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-database fa-2x opacity-25"></i>
             </div>
-            <div class="col-md-6">
-                <canvas id="registrationsChart"></canvas>
+            <div>
+              <div class="fs-3 fw-semibold">{{ @$totalDataPurchaseInGB }} GB</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Data Purchase</div>
             </div>
-        </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-phone fa-2x opacity-25"></i>
+            </div>
+            <div>
+              <div class="fs-3 fw-semibold">&#x20A6;-</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Today's Airtime Purchase</div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-xl-3">
+        <a class="block block-rounded block-link-shadow text-end" href="javascript:void(0)">
+          <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center">
+            <div class="d-none d-sm-block">
+              <i class="fa fa-money fa-2x opacity-25"></i>
+            </div>
+            <div>
+              <div class="fs-3 fw-semibold">&#x20A6;{{ @$TotalFunding }}</div>
+              <div class="fs-sm fw-semibold text-uppercase text-muted">Total Funding</div>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
+    
+    <!-- User Registrations Graph -->
+    <div class="row">
+      <div class="col-12">
+        <div class="block block-rounded block-link-shadow">
+          <div class="block-content">
+            <h3 class="block-title">User Registrations (Last 15 Days)</h3>
+            <canvas id="userRegistrationsChart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Data Purchases Graph -->
+    <div class="row">
+      <div class="col-12">
+        <div class="block block-rounded block-link-shadow">
+          <div class="block-content">
+            <h3 class="block-title">Data Purchases (Last 15 Days)</h3>
+            <canvas id="dataPurchasesChart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Funding Transactions Graph -->
+    <div class="row">
+      <div class="col-12">
+        <div class="block block-rounded block-link-shadow">
+          <div class="block-content">
+            <h3 class="block-title">Funding Transactions (Last 15 Days)</h3>
+            <canvas id="fundingTransactionsChart"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </main>
 
-@endsection
-
-@section('js')
+<!-- Include Chart.js Library -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <script>
-    var ctxFunding = document.getElementById('fundingChart').getContext('2d');
-    var ctxRegistrations = document.getElementById('registrationsChart').getContext('2d');
-    var ctxDataPurchases = document.getElementById('dataPurchasesChart').getContext('2d');
+  document.addEventListener('DOMContentLoaded', function () {
+    const dates = @json($dates);
+    const userRegistrations = @json($userRegistrations);
+    const dataPurchasesInGB = @json($dataPurchasesInGB);
+    const fundingTransactions = @json($fundingTransactions);
 
-    var dataPurchasesChart = new Chart(ctxDataPurchases, {
-        type: 'line',
-        data: {
-            labels: @json($dates),
-            datasets: [{
-                label: 'Data Purchases',
-                data: @json($dataPurchases),
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+    // User Registrations Chart
+    new Chart(document.getElementById('userRegistrationsChart').getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: 'User Registrations',
+          data: userRegistrations,
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          fill: true,
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Date'
             }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Registrations'
+            }
+          }
         }
+      }
     });
 
-    var fundingChart = new Chart(ctxFunding, {
-        type: 'line',
-        data: {
-            labels: @json($dates),
-            datasets: [{
-                label: 'Total Funding',
-                data: @json($funding),
-                backgroundColor: 'rgba(255, 159, 64, 0.2)',
-                borderColor: 'rgba(255, 159, 64, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+    // Data Purchases Chart
+    new Chart(document.getElementById('dataPurchasesChart').getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: 'Data Purchases (GB)',
+          data: dataPurchasesInGB,
+          borderColor: 'rgba(153, 102, 255, 1)',
+          backgroundColor: 'rgba(153, 102, 255, 0.2)',
+          fill: true,
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Date'
             }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Data Purchases (GB)'
+            }
+          }
         }
+      }
     });
 
-    var registrationsChart = new Chart(ctxRegistrations, {
-        type: 'line',
-        data: {
-            labels: @json($dates),
-            datasets: [{
-                label: 'Registrations',
-                data: @json($registrations),
-                backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                borderColor: 'rgba(153, 102, 255, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+    // Funding Transactions Chart
+    new Chart(document.getElementById('fundingTransactionsChart').getContext('2d'), {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: 'Funding Transactions (₦)',
+          data: fundingTransactions,
+          borderColor: 'rgba(255, 159, 64, 1)',
+          backgroundColor: 'rgba(255, 159, 64, 0.2)',
+          fill: true,
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Date'
             }
+          },
+          y: {
+            display: true,
+            title: {
+              display: true,
+              text: 'Funding Transactions (₦)'
+            }
+          }
         }
+      }
     });
+  });
 </script>
+
 @endsection
